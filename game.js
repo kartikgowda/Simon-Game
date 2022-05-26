3bd835e7cd7a8ec43d6daf1998bb2afdef806432
Kartik <kartik.gowda55@gmail.com>
2022-05-26 19:59:36 +1000

    Comparing Arrays & Adding Visuals to display feedback

--- game.js
+++ game.js
@@ -1,41 +1,35 @@
-let gamePattern = [];
+const gamePattern = [];
 let userClickedPattern = [];
 let level = 0;
+let started = false;
+let wrongSound = 'wrong';
+newGame();
 
 //? Button Colours
 let buttonColours = ['red', 'blue', 'green', 'yellow'];
 
 //* jQuery
-$(document).keydown(function () {
-  //? Selecting buttonColours array element(colour) with the random number created
-  $('h1').text('Level ' + level);
-  nextSequence();
-  // let randomChosenColour = buttonColours[nextSequence()];
-  // gamePattern.push(randomChosenColour);
-});
+function newGame() {
+  $(document).keydown(function () {
+    if (!started) {
+      //? Selecting buttonColours array element(colour) with the random number created
+      nextSequence();
+      $('h1').text('Level ' + level);
+      started = true;
+    }
+  });
+}
 
+// if (started) {
 $('.btn').click(function () {
   let userChosenColour = $(this).attr('id');
   userClickedPattern.push(userChosenColour);
   console.log(userClickedPattern);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
-
-//? Playing Sounds with the randomChosenColour
-function playSound(colour) {
-  let audio = new Audio('/sounds/' + colour + '.mp3');
-  audio.play();
-}
-
-// //? Button Press Animation
-function animatePress(buttonColour) {
-  $('#' + buttonColour).addClass('pressed');
-  setTimeout(() => {
-    $('#' + buttonColour).removeClass('pressed');
-  }, 75);
-}
-
+// }
 //? Creating random next sequence
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
@@ -50,4 +44,37 @@ function nextSequence() {
 
   $('h1').text('Level ' + level);
   // return randomNumber;
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
+    console.log('Success');
+
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+      userClickedPattern = [];
+    }
+  } else {
+    console.log('Failure, useless trash cant even get Simon right');
+    $('h1').text('Wrong Answer, Press any key to restart the game');
+    playSound(wrongSound);
+    $('body').addClass('game-over');
+    newGame();
+  }
+}
+
+//? Playing Sounds with the randomChosenColour
+function playSound(colour) {
+  let audio = new Audio('/sounds/' + colour + '.mp3');
+  audio.play();
+}
+
+// //? Button Press Animation
+function animatePress(buttonColour) {
+  $('#' + buttonColour).addClass('pressed');
+  setTimeout(() => {
+    $('#' + buttonColour).removeClass('pressed');
+  }, 75);
 }
