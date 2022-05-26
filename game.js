8c29819f0da0350dfbcc3f60a225c7aa0138ec95
Kartik <kartik.gowda55@gmail.com>
2022-05-26 17:32:51 +1000

    Adding Sequence and Levels

--- game.js
+++ game.js
@@ -1,31 +1,18 @@
 let gamePattern = [];
 let userClickedPattern = [];
+let level = 0;
 
-//? Button Color Array
+//? Button Colours
 let buttonColours = ['red', 'blue', 'green', 'yellow'];
 
-//? Creating random next sequence
-function getNextSequence() {
-  let randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
-}
-
-//? Selecting buttonColours array element(colour) with the random number created
-let randomChosenColour = buttonColours[getNextSequence()];
-
-gamePattern.push(randomChosenColour);
-
-console.log(randomChosenColour);
-
 //* jQuery
-
-//? Fade Out & Fade In after Random Color is Chosen
-$('#' + randomChosenColour)
-  .fadeOut(200)
-  .fadeIn(200);
-
-// $(O).triggerHandler(eventType, { name: value });
-$(document).ready(function () {});
+$(document).keydown(function () {
+  //? Selecting buttonColours array element(colour) with the random number created
+  $('h1').text('Level ' + level);
+  nextSequence();
+  // let randomChosenColour = buttonColours[nextSequence()];
+  // gamePattern.push(randomChosenColour);
+});
 
 $('.btn').click(function () {
   let userChosenColour = $(this).attr('id');
@@ -40,36 +27,27 @@ function playSound(colour) {
   let audio = new Audio('/sounds/' + colour + '.mp3');
   audio.play();
 }
-// //? Button Press Animation
 
+// //? Button Press Animation
 function animatePress(buttonColour) {
   $('#' + buttonColour).addClass('pressed');
   setTimeout(() => {
     $('#' + buttonColour).removeClass('pressed');
   }, 75);
 }
-// $('#' + randomChosenColour).click(function (e) {
-//   e.preventDefault();
-//   // colour = this.classList[1];
-//   // playSound(colour);
 
-// $('#' + randomChosenColour).click(function () {
-//   $(this).add('pressed');
-//   // setTimeout(() => {
-//   //   // console.log(this);
-//   //   $(this).remove('pressed');
-//   // }, 1000);
-// });
-// console.log($(randomChosenColour));
-// function animatePress(buttonColour) {
-//   $('#' + buttonColour).fadeOut(200);
-//   $('#' + buttonColour).fadeIn(200);
-// }
-// $(document).on('click', function () {
-// $('input[type="button"]').click(function () {
-//   console.log('pressed');
-//   let userChosenColour = e.classList[1];
-//   userClickedPattern.push(userChosenColour);
-//   console.log(userClickedPattern);
-// });
-// });
+//? Creating random next sequence
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  playSound(randomChosenColour);
+  gamePattern.push(randomChosenColour);
+  //? Fade Out & Fade In Animation after Random Color is Chosen
+  $('#' + randomChosenColour)
+    .fadeOut(200)
+    .fadeIn(200);
+  level = gamePattern.length;
+
+  $('h1').text('Level ' + level);
+  // return randomNumber;
+}
